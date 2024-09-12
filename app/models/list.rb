3f82b22fbbd365ad54c22867ff2ac519ff0f167b
Chaewan Shin <chaewanshin@gmail.com>
2024-09-13 01:04:23 +0900

    model further updates

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
   self.table_name = "rails_wl_lists"
-  has_many :bookmarks, dependent: :destroy
-  has_many :movies, through: :bookmarks
+  has_many :bookmarks, foreign_key: "rails_wl_list_id", dependent: :destroy
+  has_many :movies, foreign_key: "rails_wl_movie_id", through: :bookmarks
   has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   self.table_name = "rails_wl_movies"
-  has_many :bookmarks
+  has_many :bookmarks, foreign_key: "rails_wl_movie_id"
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
