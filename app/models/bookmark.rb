4a4daec40ad887c56cda68bb47746b4ff09d16c2
Chaewan Shin <chaewanshin@gmail.com>
2024-09-13 00:37:47 +0900

    model updates

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,10 +1,18 @@
 class Bookmark < ApplicationRecord
-  belongs_to :movie
-  belongs_to :list
+  self.table_name = "rails_wl_bookmark"
+  belongs_to :movie, foreign_key: "rails_wl_movie_id"
+  belongs_to :list, foreign_key: "rails_wl_list_id"
 
   validates :comment, length: { minimum: 6 }
-  validates :movie, uniqueness: {
-    scope: :list,
+  validates :rails_wl_movie_id, uniqueness: {
+    scope: :rails_wl_list_id,
     message: 'This movie exist'
   }
+
+  def movie_id
+    rails_wl_movie_id
+  end
+  def list_id
+    rails_wl_list_id
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  self.table_name = "rails_wl_movie"
   has_many :bookmarks
 
   validates :title, presence: true, uniqueness: true
